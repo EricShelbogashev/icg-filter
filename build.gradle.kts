0cfe47e84a1063fa2d6d79e0da7b1e3ad568dda2
Eric Shelbogashev <e.shelbogashev@trueengineering.ru>
2024-03-07 13:42:27 +0700

    removed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,6 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven{
-        url = uri("https://jitpack.io")
-    }
-}
-
-dependencies {
-    implementation("com.github.EricShelbogashev:icg-filter:main-SNAPSHOT")
-//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-//    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
