64b37a1918dc255b85627449e2714aaf0f0a570d
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2024-03-21 16:16:22 +0700

    Refactor FSDithering

--- src/main/java/model/filter/leonid/FSDithering.java
+++ src/main/java/model/filter/leonid/FSDithering.java
@@ -16,51 +16,20 @@ public class FSDithering extends CustomFilter {
         this.quantizationGreen = quantizationGreen;
     }
 
-    /*@Override
-    protected int apply(Image image, int x, int y) {
-        int oldRed = image.red(x, y);
-        int oldGreen = image.green(x, y);
-        int oldBlue = image.blue(x, y);
-        float errRed = 0;
-        float errGreen = 0;
-        float errBlue = 0;
-        int[] neighbours = {image.color(x - 1, y), image.color(x + 1, y -1), image.color(x, y-1), image.color(x-1, y-1)};
-        for (int i = 0; i < 4; i++){
-            errRed += (oldRed - ColorUtils.red(neighbours[i])) * koef[i];
-            errGreen += (oldGreen - ColorUtils.green(neighbours[i])) * koef[i];
-            errBlue += (oldBlue- ColorUtils.blue(neighbours[i])) * koef[i];
-        }
-        int resultRed = ColorUtils.findClosestColor(oldRed + (int)errRed, quantizationRed);
-        int resultGreen = ColorUtils.findClosestColor(oldGreen + (int)errGreen, quantizationGreen);
-        int resultBlue = ColorUtils.findClosestColor(oldBlue + (int)errBlue, quantizationBlue);
-        return ColorUtils.rgb(resultRed, resultGreen, resultBlue, image.alpha(x,y));
-    }*/
-
-    /*for each y from top to bottom do
-            for each x from left to right do
-    oldpixel := pixels[x][y]
-    newpixel := find_closest_palette_color(oldpixel)
-    pixels[x][y] := newpixel
-    quant_error := oldpixel - newpixel
-    pixels[x + 1][y    ] := pixels[x + 1][y    ] + quant_error × 7 / 16
-    pixels[x - 1][y + 1] := pixels[x - 1][y + 1] + quant_error × 3 / 16
-    pixels[x    ][y + 1] := pixels[x    ][y + 1] + quant_error × 5 / 16
-    pixels[x + 1][y + 1] := pixels[x + 1][y + 1] + quant_error × 1 / 16*/
-
 
     float chooseFactor(int xOffset, int yOffset) {
         if (xOffset == 1 && yOffset == 0) {
             return 7.0f / 16;
         }
         else if (xOffset == -1 && yOffset == 1) {
-            return 3.0f / 16;
+            return 1.0f / 16;
         }
         else if (xOffset == 0 && yOffset == 1) {
             return 5.0f / 16;
         }
 
         else if (xOffset == 1 && yOffset == 1) {
-            return 1.0f / 16;
+            return 3.0f / 16;
         }
 
         return 0;
