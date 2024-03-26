f09a29f5b193494210557ef7bd874cac0be91377
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2024-03-26 13:08:58 +0700

    FillColorFilter converted using ColorUtils

--- src/main/java/model/filter/darya/FillColorFilter.java
+++ src/main/java/model/filter/darya/FillColorFilter.java
@@ -2,6 +2,7 @@ package model.filter.darya;
 
 import core.filter.Image;
 import core.filter.MatrixFilter;
+import model.filter.leonid.ColorUtils;
 
 public class FillColorFilter extends MatrixFilter {
     public FillColorFilter() {
@@ -10,11 +11,11 @@ public class FillColorFilter extends MatrixFilter {
 
     @Override
     protected int apply(Image image, int x, int y) {
-        int alpha = (image.color(x, y) >> 24) & 0xFF;
-        int r = ((alpha & 0xFF) << 24) |
-                ((((image.red(x, y) / 8) * 10 + 40) & 0xFF) << 16) |
-                ((((image.green(x, y) / 8) * 20 + 40) & 0xFF) << 8) |
-                ((((image.blue(x, y) / 8) * 40 + 40) & 0xFF));
-        return r;
+        int alpha = image.alpha(x, y);
+        return ColorUtils.rgb(
+                (image.red(x, y) / 8) * 10 + 40,
+                (image.green(x, y) / 8) * 20 + 40,
+                (image.blue(x, y) / 8) * 40 + 40,
+                alpha);
     }
 }
